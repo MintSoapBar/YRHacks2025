835d1dbf596de817c923f72569e24451ad46d425
MintSoapBar <matthewzhengshuo@gmail.com>
2025-04-07 19:44:02 -0400

    Added Duration.java

---
+++ Duration.java
@@ -0,0 +1,55 @@
+
+//class to convert milliseconds time into human readable time
+//eg displays 12000ms as 12 seconds
+public class Duration {
+    final static long second = 1000;
+    final static long minute = second*60;
+    final static long hour = minute*60;
+    final static long day = hour*24;
+    final static long week = day*7;
+    final static long month = day*30;
+    final static long year = (long) (day*365.25);
+
+    long time; //in milliseconds
+    String textLong;
+    String textShort;
+
+    public void setTime(long t) {
+        time = t;
+
+        if (time < minute) {
+            textLong = time/second + " second" + (time >= 2*second || time < second? "s": "");
+            textShort = time/second + "s";
+        } else if (time < hour) {
+            textLong = time/minute + " minutes" + (time >= 2*minute? "s": "");
+            textShort = time/minute + "min.";
+        } else if (time < day) {
+            textLong = time/hour + " hour" + (time >= 2*hour? "s": "");
+            textShort = time/hour + "hr.";
+        } else if (time < week) {
+            textLong = time/day + " day" + (time >= 2*day? "s": "");
+            textShort = time/day + "d";
+        } else if (time < month) {
+            textLong = time/week + " week" + (time >= 2*week? "s": "");
+            textShort = time/week + "w";
+        } else if (time < year) {
+            textLong = time/month + " month" + (time >= 2*month? "s": "");
+            textShort = time/month + "mnth.";
+        } else {
+            textLong = time/year + " year" + (time >= 2*year? "s": "");
+            textShort = time/year + "yr.";
+        }
+    }
+
+    public Duration(long t) {
+        setTime(t);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Duration(12345));
+    }
+
+    public String toString() {
+        return textLong;
+    }
+}
