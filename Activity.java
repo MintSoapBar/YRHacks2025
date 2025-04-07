e6e8c70bce10daccabc1e2c8edbece8901b273d5
erniesource <li.ernie.bernie@gmail.com>
2025-04-07 18:17:25 -0400

    Initialized Task and ACtivity?

--- Activity.java
+++ Activity.java
@@ -1,4 +1,14 @@
 
 public class Activity {
+    String name;
+    Time start;
+    int length;
+    Time end;
 
+    public Activity(String name, Time start, int length) {
+        this.name = name;
+        this.start = start;
+        this.length = length;
+        // this.end = new Time(start.getHour(), start.getMinute() + length);
+    }
 }
--- Task.java
+++ Task.java
@@ -1,5 +1,12 @@
 
 public class Task {
+    String name;
     Time dueDate;
     int length;
+
+    public Task(String name, Time dueDate, int length) {
+        this.name = name;
+        this.dueDate = dueDate;
+        this.length = length;
+    }
 }
