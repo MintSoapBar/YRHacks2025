245646f6a16851659493435844e7e209c273bbcb
erniesource <li.ernie.bernie@gmail.com>
2025-04-08 09:43:12 -0400

    Added sorting of tasks-ish

--- classes/Task.java
+++ classes/Task.java
@@ -1,22 +1,26 @@
 package classes;
 
-public class Task {
+public class Task implements Comparable<Task> {
     String name;
     String description;
     Time dueDate;
     long length;
     Double priority;
 
-    public Task(String name, String description, Time dueDate, Double priority) {
+    public Task(String name, String description, Time dueDate, long length, Double priority) {
         this.name = name;
         this.description = description;
         this.dueDate = dueDate;
-        this.length = dueDate.time - System.currentTimeMillis();
+        this.length = length;
         this.priority = priority;
     }
 
     public String toString() {
         return String.format("Name: %s%nDescription: %s%nDue Date: %s%nLength: %d%nPriority: %f%n", 
             name, description, dueDate, length, priority);
+    }
+
+    public int compareTo(Task task) {
+        return this.priority.compareTo(task.priority);
     }
 }
