f7f9d54960525e1e1e837ebbf88fc3c1207df0ed
MintSoapBar <matthewzhengshuo@gmail.com>
2025-04-07 21:35:48 -0400

    Changed import ...BufferedImage to ...* in Driver class

--- classes/Driver.java
+++ classes/Driver.java
@@ -3,7 +3,7 @@ package classes;
 import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
+import java.awt.image.*;
 import java.io.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
