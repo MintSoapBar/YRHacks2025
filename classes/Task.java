0305b48c7865c0de14c3e1cd4cd5e2b024742cb2
CaptainBoo <eddiethebest06@gmail.com>
2025-04-07 21:45:20 -0400

    added prototype task group and tasks only

--- classes/Task.java
+++ classes/Task.java
@@ -2,14 +2,16 @@ package classes;
 
 public class Task {
     String name;
-    int id;
-    
     Time dueDate;
     long length;
+    String description;
+    Double priority;
 
-    public Task(String name, Time dueDate, long length) {
+    public Task(String name, Time dueDate, String description, Double priority) {
         this.name = name;
         this.dueDate = dueDate;
-        this.length = length;
+        this.length = dueDate.time - System.currentTimeMillis();
+        this.description = description;
+        this.priority = priority;
     }
 }
--- classes/TaskGroup.java
+++ classes/TaskGroup.java
@@ -4,26 +4,35 @@ import java.util.ArrayList;
 
 public class TaskGroup {
     String name;
-    int id;
-
 	ArrayList<Task> tasks = new ArrayList<>();
-    
 	Time startDate;
 	Time dueDate;
 	Double priority;
+	String description;
 
 	// Constructor
-	public TaskGroup(String name, Time dueDate, Time startDate, Double priority) {
+	public TaskGroup(String name, Time startDate, Time dueDate, String description, Double priority) {
 		this.name = name;
 		this.startDate = startDate;
 		this.dueDate = dueDate;
+		this.description = description;
 		this.priority = priority;
 	}
 
 	public void addTask(Task task) {
 		tasks.add(task);
 	}
+	public Task getTask(int index) {
+		return tasks.get(index);
+	}
+	public ArrayList<Task> getTasks() {
+		return tasks;
+	}
 	public void removeTask(Task task) {
 		tasks.remove(task);
+	}
+
+	public String toString() {
+		return String.format("%s%n%s%n%s%n%f%n", name, startDate, dueDate, priority);
 	}
 }
