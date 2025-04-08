acb68de6c81def1a35adb338666a306ec65da9ad
MintSoapBar <matthewzhengshuo@gmail.com>
2025-04-08 11:46:09 -0400

    made Frame.java a superclass

--- classes/Button.java
+++ classes/Button.java
@@ -1,64 +1,32 @@
 package classes;
+
 import java.awt.*;
+import java.awt.image.*;;
 
-public class Button {
-    int x, y, width, height;
-    Color backgroundColor = new Color(255, 255, 255);
-    Color borderColor = new Color(0, 0, 0);
-    String text;
-
+public class Button extends Frame {
     Frame parent;
 
     public boolean isPosInBounds(int posX, int posY, int ox, int oy) {
         return posX >= x + ox && posX <= x + ox + width && posY >= y + oy && posY <= y + oy + height;
     }
-
-    public void render(Graphics g, int ox, int oy) {
-        if (backgroundColor != null) {
-            g.setColor(backgroundColor);
-            g.fillRect(x + ox, y + oy, width, height);
-        }
-
-        if (borderColor != null) {
-            g.setColor(borderColor);
-            g.drawRect(x + ox, y + oy, width, height);
-        }
-
-        g.drawString(
-            text, 
-            x + ox + (width - g.getFontMetrics().stringWidth(text)) / 2, 
-            y + oy + (int) (height/2 + g.getFontMetrics().getHeight() * 0.4)
-            );
-    }
-
-    public void render(Graphics g) {
-        render(g, 0, 0);
-    }
-
-    public Button(int x, int y, int width, int height, Color backgroundColor, Color borderColor, String text) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.text = text;
-        this.backgroundColor = backgroundColor;
-        this.borderColor = borderColor;
+    
+    public Button(int x, int y, int width, int height, BufferedImage image) {
+        super(x, y, width, height, image);
     }
 
     public Button(int x, int y, int width, int height, Color backgroundColor, String text) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.text = text;
-        this.backgroundColor = backgroundColor;
+        super(x, y, width, height, backgroundColor, text);
+    }
+
+    public Button(int x, int y, int width, int height, Color backgroundColor) {
+        super(x, y, width, height, backgroundColor);
     }
 
     public Button(int x, int y, int width, int height, String text) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.text = text;
+        super(x, y, width, height, text);
+    }
+
+    public Button(int x, int y, int width, int height) {
+        super(x, y, width, height);
     }
 }
